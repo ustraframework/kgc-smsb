082908ed60089dc1243bb0c6a013e6d517f77667
skw0917 <65753907+skw0917@users.noreply.github.com>
2023-11-14 12:56:19 +0900

    LegacyAPI, POS backend 소스 추가

---
+++ java/pos/src/main/java/kr/co/kgc/smsb/pos/config/SmsbPosConfigProperties.java
@@ -0,0 +1,15 @@
+package kr.co.kgc.smsb.pos.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties( value = SmsbPosConfigProperties.PREFIX, ignoreUnknownFields = true )
+public class SmsbPosConfigProperties {
+	public static final String PREFIX = "smsb.pos";
+
+	private String sampleProp;
+}
---
+++ java/pos/src/main/java/kr/co/kgc/smsb/pos/sample/service/SampleService.java
@@ -0,0 +1,5 @@
+package kr.co.kgc.smsb.pos.sample.service;
+
+public class SampleService {
+
+}
