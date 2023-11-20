f44085cb95699d66b96a25d1e78d068f2f70a611
it1403 <anko@gsitm.com>
2023-11-20 10:15:31 +0900

    회원가입 인증 페이지 작업

--- node/fo/layouts/default.vue
+++ node/fo/layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
-  <Header />
-  <main class="container !max-w-full bg-white">
-    <slot></slot>
-  </main>
+  <div class="min-w-[1280px]">
+    <Header />
+    <main class="container !max-w-full bg-white flex flex-col">
+      <slot></slot>
+    </main>
+  </div>
 </template>
 <script lang="ts" setup>
 import Header from '@/components/layouts/header.vue'
 </script>
 <script lang="ts"></script>
 <style lang="scss">
-.contents {
-}
+
 </style>
