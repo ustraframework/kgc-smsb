6f671a1c84a40418f3de658492f7ec2531a45e43
김명욱 <anko@gsitm.com>
2023-11-21 15:04:34 +0900

    form components 작업

--- node/fo/pages/docs/forms.vue
+++ node/fo/pages/docs/forms.vue
@@ -18,7 +18,7 @@
         <InputText class="p-invalid" v-model="value" type="text" size="large" placeholder="Error" />
       </div>
     </div>
-    
+
     <div class="mt-[40px]">
       <h2 class="text-lg font-bold mb-[20px]">Password</h2>
       <div class="flex items-end gap-[38px] mt-[10px]">
@@ -27,7 +27,7 @@
         <Password inputClass="p-inputtext-lg" v-model="value" :feedback="false" toggleMask placeholder="Password" />
       </div>
     </div>
-    
+
     <div class="mt-[40px]">
       <h2 class="text-lg font-bold mb-[20px]">Search Input</h2>
       <div class="flex items-end gap-[38px]">
@@ -63,46 +63,163 @@
     <div class="mt-[40px]">
       <h2 class="text-lg font-bold mb-[20px]">Dropdown</h2>
       <div class="flex items-end gap-[38px]">
-        <Dropdown inputClass="p-inputtext-sm" panelClass="p-dropdown-panel-sm" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
+        <Dropdown
+          inputClass="p-inputtext-sm"
+          panelClass="p-dropdown-panel-sm"
+          v-model="selectedCity"
+          :options="cities"
+          optionLabel="name"
+          placeholder="Select A"
+          class="w-[200px]"
+        />
         <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
         <Dropdown inputClass="p-inputtext-lg" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
       </div>
       <div class="flex items-end gap-[38px] mt-[10px]">
-        <Dropdown disabled inputClass="p-inputtext-sm" panelClass="p-dropdown-panel-sm" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
+        <Dropdown
+          disabled
+          inputClass="p-inputtext-sm"
+          panelClass="p-dropdown-panel-sm"
+          v-model="selectedCity"
+          :options="cities"
+          optionLabel="name"
+          placeholder="Select A"
+          class="w-[200px]"
+        />
         <Dropdown disabled v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
-        <Dropdown disabled inputClass="p-inputtext-lg" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px]" />
+        <Dropdown
+          disabled
+          inputClass="p-inputtext-lg"
+          v-model="selectedCity"
+          :options="cities"
+          optionLabel="name"
+          placeholder="Select A"
+          class="w-[200px]"
+        />
       </div>
       <div class="flex items-end gap-[38px] mt-[10px]">
-        <Dropdown inputClass="p-inputtext-sm" panelClass="p-dropdown-panel-sm" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px] p-invalid" />
+        <Dropdown
+          inputClass="p-inputtext-sm"
+          panelClass="p-dropdown-panel-sm"
+          v-model="selectedCity"
+          :options="cities"
+          optionLabel="name"
+          placeholder="Select A"
+          class="w-[200px] p-invalid"
+        />
         <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px] p-invalid" />
-        <Dropdown inputClass="p-inputtext-lg" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select A" class="w-[200px] p-invalid" />
+        <Dropdown
+          inputClass="p-inputtext-lg"
+          v-model="selectedCity"
+          :options="cities"
+          optionLabel="name"
+          placeholder="Select A"
+          class="w-[200px] p-invalid"
+        />
+      </div>
+    </div>
+
+    <div class="mt-[40px]">
+      <h2 class="text-lg font-bold mb-[20px]">Checkbox</h2>
+      <div class="flex flex-wrap items-center gap-10">
+        <div class="flex items-center">
+          <Checkbox checked v-model="pizza" inputId="ingredient1" name="pizza" value="체크" />
+          <label for="ingredient1"> 체크 </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox disabled v-model="pizza" inputId="ingredient2" name="pizza" value="Disabled" />
+          <label for="ingredient2"> Disabled </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox class="p-checkbox-box-lg" v-model="pizza" inputId="ingredient3" name="pizza" value="Large" />
+          <label for="ingredient3"> Large </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox disabled class="p-checkbox-box-lg" v-model="pizza" inputId="ingredient4" name="pizza" value="Large Disabled" />
+          <label for="ingredient4"> Large Disabled </label>
+        </div>
+      </div>
+      <div class="flex flex-wrap items-center gap-10 mt-[20px]">
+        <div class="flex items-center">
+          <Checkbox class="non-rounded" v-model="pizza" inputId="ingredient5" name="pizza" value="Cheese" />
+          <label for="ingredient5"> Cheese </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox disabled class="non-rounded" v-model="pizza" inputId="ingredient6" name="pizza" value="disabled" />
+          <label for="ingredient6"> disabled </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox class="non-rounded p-checkbox-box-lg" v-model="pizza" inputId="ingredient7" name="pizza" value="Pepper" />
+          <label for="ingredient7"> Pepper </label>
+        </div>
+        <div class="flex items-center">
+          <Checkbox class="non-rounded p-checkbox-box-lg" v-model="pizza" inputId="ingredient8" name="pizza" value="Onion" />
+          <label for="ingredient8"> Onion </label>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-[40px]">
+      <h2 class="text-lg font-bold mb-[20px]">Radio</h2>
+      <div class="flex flex-wrap gap-3">
+        <div class="flex items-center">
+          <RadioButton v-model="ingredient" inputId="ingredient11" name="rice" value="Radio1" />
+          <label for="ingredient11">Radio1</label>
+        </div>
+        <div class="flex items-center">
+          <RadioButton v-model="ingredient" inputId="ingredient12" name="rice" value="Radio2" />
+          <label for="ingredient12">Radio2</label>
+        </div>
+        <div class="flex items-center">
+          <RadioButton class="p-radiobutton-box-lg" v-model="ingredient" inputId="ingredient13" name="rice" value="Radio3" />
+          <label for="ingredient13">Radio3</label>
+        </div>
+        <div class="flex items-center">
+          <RadioButton class="p-radiobutton-box-lg" disabled v-model="ingredient" inputId="ingredient14" name="rice" value="Radio4" />
+          <label for="ingredient14">Radio4</label>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-[40px]">
+      <h2 class="text-lg font-bold mb-[20px]">Switch Button</h2>
+      <div>
+        <InputSwitch v-model="checked" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const value = ref(null);
-const value1 = ref(null);
-const value2 = ref(null);
-const value3 = ref(null);
-const value4 = ref(null);
-const value5 = ref(null);
-const value6 = ref(null);
+// input
+const value = ref(null)
+const value1 = ref(null)
+const value2 = ref(null)
+const value3 = ref(null)
+const value4 = ref(null)
+const value5 = ref(null)
+const value6 = ref(null)
 
 // dropdown
-const selectedCity = ref();
+const selectedCity = ref()
 const cities = ref([
   { name: 'New York', code: 'NY' },
   { name: 'Rome', code: 'RM' },
   { name: 'London', code: 'LDN' },
   { name: 'Istanbul', code: 'IST' },
-  { name: 'Paris', code: 'PRS' }
-]);
+  { name: 'Paris', code: 'PRS' },
+])
+
+// checkbox
+const pizza = ref()
+
+// radio
+const ingredient = ref('')
+
+// switch
+const checked = ref(false)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
