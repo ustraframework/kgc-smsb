13cd8f745b6494a9e2351ae1245804267cd5fd6b
hainyiseul <yiseul@gsitm.com>
2023-11-23 11:46:47 +0900

    template 페이지 작업

---
+++ node/bo/pages/template/grid.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="card is-sub is-search">
+    <div class="card-header">
+      <h1 class="page-title">
+        <span>회원정보 변경 이력 조회</span>
+      </h1>
+    </div>
+    <div class="card-body">
+      <form action="" class="form">
+        <table class="table is-search is-fullwidth">
+          <colgroup>
+            <col style="width: 130px;">
+            <col>
+            <col style="width: 130px;">
+            <col>
+            <col style="width: 130px;">
+            <col>
+            <col style="width: 170px;">
+          </colgroup>
+          <tbody>
+            <tr>
+              <th><span class="is-required">사용채널</span></th>
+              <td>
+                <WjComboBox :itemsSource="items" selectedValuePath="value" displayMemberPath="text" />
+              </td>
+              <th><span class="is-required">인증일자</span></th>
+              <td>
+                <UDatePeriodBox v-model:start="start" v-model:end="end" />
+              </td>
+              <th><span class="is-required">인증채널</span></th>
+              <td>
+                <UWjComboBox :itemsSource="items" displayMemberPath="text"/>
+              </td>
+              <td rowspan="2">
+                <div class="buttons is-search">
+                  <UButton text="조회" type="is-search"/>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>회원번호/명</th>
+              <td>
+                <UBox>
+                  <UTextBox type="icon"/>
+                  <UTextBox></UTextBox>
+                </UBox>
+              </td>
+              <th></th>
+              <td></td>
+              <th></th>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  </div>
+
+  <!-- ------------------------------------------------------------------ -->
+  <div class="columns has-gap">
+    <div class="card is-sub">
+      <div class="card-body">
+        <div class="table-title-wrap">
+          <h2 class="table-title">
+            <span class="data-count">총<span>24</span>건</span>
+          </h2>
+          <div class="buttons">
+            <UButton text="엑셀 다운로드" icon="excel" />
+          </div>
+        </div>
+        
+        <div class="field-grid">
+          <WjFlexGrid :itemsSource="itemsSourceGrid">
+            <WjFlexGridColumn header="No" binding="col1" width="*" />
+            <WjFlexGridColumn header="회원번호" binding="col1" width="*" />
+            <WjFlexGridColumn header="채널" binding="col1" width="*" />
+            <WjFlexGridColumn header="회원명" binding="col1" width="*" />
+            <WjFlexGridColumn header="컬럼라벨" binding="col1" width="*" />
+            <WjFlexGridColumn header="변경전데이터" binding="col1" width="*" />
+            <WjFlexGridColumn header="변경후데이터" binding="col1" width="*" />
+            <WjFlexGridColumn header="변경사유" binding="col1" width="*" />
+            <WjFlexGridColumn header="컬럼명" binding="col1" width="*" />
+            <WjFlexGridColumn header="수정자" binding="col1" width="*" />
+            <WjFlexGridColumn header="수정일시" binding="col1" width="*" />
+          </WjFlexGrid>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { WjComboBox } from '#ustra/nuxt-wijmo/components'
+import { WjFlexGrid, WjFlexGridColumn, WjFlexGridCellTemplate, WjInputDate } from '#ustra/nuxt-wijmo/components';
+
+const grid = useWijmoFlexGrid({
+isReadOnly: false,
+})  
+
+const items = [
+  { value: '01', text: '항목1' },
+  { value: '02', text: '항목2' },
+]
+
+definePageMeta({
+auth: {
+  required: false,
+},
+})
+const { collectionView, loadPageData, pageSize, totalRecords, currentPageNo } = usePaginationCollectionView((pageNo, orders) => {
+  return useOnError(
+    async () => {
+      const service = useUstraInterfaceService()
+      const result = await service.getIntefaceHistories({
+        header: {
+          currentPage: pageNo,
+          pageSize: pageSize.value,
+          orders,
+        },
+        searchValue: {},
+      })
+      return {
+        data: result.body,
+        totalRecords: result.header.totalRecords,
+      }
+    },
+    { message: '데이터 조회 중 오류가 발생하였습니다.' },
+  )()
+})
+
+const itemsSourceGrid = ref([
+{ col1: '텍스트' },
+{ col1: '텍스트2'},
+{ col1: '텍스트3'},
+]);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
