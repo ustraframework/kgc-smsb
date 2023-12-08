67ed8ad8669bfbfb1d2143ffb8bb0686c5249136
hanyiseul <yiseul@gsitm.com>
2023-12-08 11:18:09 +0900

    레이아웃 수정

--- node/bo/pages/docs/layout.vue
+++ node/bo/pages/docs/layout.vue
@@ -1,231 +1,210 @@
 <template>
-  <div class="card is-sub is-search">
-    <div class="card-header">
-      <h1 class="page-title">
-        <span>본문 제목</span>
-        <div class="columns">
-          <ul aria-label="breadcrumbs" class="breadcrumbs has-chevron-separator">
-            <li class="breadcrumb"><a href="#none">인사</a></li>
-            <li class="breadcrumb"><a href="#none">인사관리</a></li>
-            <li aria-current="page" class="breadcrumb is-active"><a>인사코드</a></li>
-          </ul>
-        </div>
-      </h1>
-    </div>
-    <div class="card-body">
-      <form action="" class="form">
-        <table class="table is-search is-fullwidth">
-          <colgroup>
-            <col style="width: 130px;">
-            <col>
-            <col style="width: 130px;">
-            <col>
-            <col style="width: 130px;">
-            <col>
-            <col style="width: 170px;">
-          </colgroup>
-          <tbody>
-            <tr>
-              <th><span class="is-required">회원번호/명</span></th>
-              <td>
-                <UTextBox></UTextBox>
-              </td>
-              <th></th>
-              <td></td>
-              <th></th>
-              <td></td>
-              <td>
-                <div class="buttons is-search">
-                  <UButton text="조회" type="is-search"/>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
-  </div>
+  <!-- 검색영역 - 본문제목 있는 경우 -->
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub is-search"  ratio="1" >
+      <UBox class="card-header">
+        <h1 class="page-title">
+          <span>본문 제목</span>
+        </h1>
+      </UBox>
+      
+      <UFieldSet class="is-search">
+        <UFieldRow :ratio="[1, 1, 1, '170px']">
+          <UField label="검색영역" ><UTextBox></UTextBox></UField>
+          <UField blank></UField>
+          <UField blank></UField>
+          <UField blank>
+            <div class="search-btn">
+              <UButton text="조회" type="is-search"/>
+            </div>
+          </UField>
+        </UFieldRow>
+      </UFieldSet>
+    </UItem>
+  </UBox>
+  <!-- // 검색영역 - 본문제목 있는 경우 --> 
 
-  <div class="columns has-gap">
-    <div class="card is-sub" style="width: 100%">
-      <div class="card-body">
+  <!-- 검색영역 - 본문제목 없는 경우 -->
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub is-search"  ratio="1" >
+      <UFieldSet class="is-search">
+        <UFieldRow :ratio="[1, 1, 1, '170px']">
+          <UField label="검색영역" ><UTextBox></UTextBox></UField>
+          <UField blank></UField>
+          <UField blank></UField>
+          <UField blank>
+            <div class="search-btn">
+              <UButton text="조회" type="is-search"/>
+            </div>
+          </UField>
+        </UFieldRow>
+      </UFieldSet>
+    </UItem>
+  </UBox>
+  <!-- // 검색영역 - 본문제목 없는 경우 --> 
+
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub"  ratio="1" >
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col12</span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="columns has-gap">
-    <div class="card is-sub" style="width: 50%">
-      <div class="card-body">
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
+  
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col5</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card is-sub" style="width: 50%">
-      <div class="card-body">
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col5</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
 
-  <div class="columns has-gap">
-    <div class="card is-sub" style="width: 370px">
-      <div class="card-body">
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub" ratio="3">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col3</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card is-sub">
-      <div class="card-body">
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="9">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col9</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
 
-  <div class="columns has-gap">
-    <div class="card is-sub" style="width: 500px">
-      <div class="card-body">
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub" ratio="4">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col4</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card is-sub">
-      <div class="card-body">
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="8">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
           <span>col8</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
 
+  <UBox class="columns" direction="row">
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
+        <h2 class="table-title">
+          <span>col3</span>
+          <span class="data-count">총<span>24</span>건</span>
+        </h2>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
+        <h2 class="table-title">
+          <span>col3</span>
+          <span class="data-count">총<span>24</span>건</span>
+        </h2>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
+        <h2 class="table-title">
+          <span>col3</span>
+          <span class="data-count">총<span>24</span>건</span>
+        </h2>
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
 
-  <div class="columns has-gap">
-    <div class="card is-sub" style="width: 33.3%">
-      <div class="card-body">
+  <UBox class="columns" direction="row" height="500px">
+    <UItem class="card is-sub" baseSize="500px">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
-          <span>col4</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span>width size</span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card is-sub" style="width: 33.3%">
-      <div class="card-body">
+      </UBox>
+      
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+    <UItem class="card is-sub" ratio="1">
+      <UBox class="table-title-wrap">
         <h2 class="table-title">
-          <span>col4</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
+          <span>width size auto</span>
+          <span class="data-count">총<span>24</span>건</span>
         </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card is-sub" style="width: 33.3%">
-      <div class="card-body">
-        <h2 class="table-title">
-          <span>col4</span>
-          <span class="data-count">
-            총
-            <span>0</span>
-            건
-          </span>
-        </h2>
-        <div class="card-body">
-          <p>
-          Contents
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+      </UBox>
+
+      <UBox>
+        Contents
+      </UBox>
+    </UItem>
+  </UBox>
+
 </template>
 
 <script setup>
@@ -233,6 +212,7 @@ definePageMeta({
   auth: {
     required: false,
   },
+  layout: 'ustra-pub'
 })
 </script>
 
