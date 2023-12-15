d942e9cdc5ec59dabdbfbe1a33d82ff83334fd1c
it1403 <anko@gsitm.com>
2023-12-15 11:23:15 +0900

    마이페이지 사이드바 수정

--- node/fo/components/layouts/sidebar.vue
+++ node/fo/components/layouts/sidebar.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="pr-[80px]">
-    <h1 class="text-[24px] text-[#000] font-bold">마이페이지</h1>
-    <ul class="flex flex-col mt-[23px]">
-      <li class="main_list">포인트조회</li>
-      <li class="main_list">쿠폰조회</li>
-      <li class="main_list">구매내역 조회</li>
-      <li class="main_list">포인트 선물</li>
-      <li class="main_list" style="margin-bottom: 20px;">내정보 수정</li>
+  <div class="min-w-[150px] mr-[40px]">
+    <h1 class="text-2xl text-black font-bold">마이페이지</h1>
+    <ul class="flex flex-col mt-[15px]">
+      <li class="main_item">
+        <router-link to="/">포인트 조회</router-link>
+      </li>
+      <li class="main_item">
+        <router-link to="/">쿠폰 조회</router-link>
+      </li>
+      <li class="main_item">
+        <router-link to="/">구매내역 조회</router-link>
+      </li>
+      <li class="main_item">
+        <router-link to="/">포인트 선물</router-link>
+      </li>
+      <li class="main_item">
+        <a href="#none">내정보 수정</a>
 
-      <li class="sub_list">회원정보 변경</li>
-      <li class="sub_list">비밀번호 변경</li>
-      <li class="sub_list">기념일 관리</li>
-      <li class="sub_list">단골매장 관리</li>
-      <li class="sub_list">회원탈퇴</li>
+        <ul class="sub_list">
+          <li class="sub_item">
+            <router-link to="/">회원정보 변경</router-link>
+          </li>
+          <li class="sub_item">
+            <router-link to="/">비밀번호 변경</router-link>
+          </li>
+          <li class="sub_item">
+            <router-link to="/">기념일 관리</router-link>
+          </li>
+          <li class="sub_item">
+            <router-link to="/">단골매장 관리</router-link>
+          </li>
+          <li class="sub_item">
+            <router-link to="/">회원 탈퇴</router-link>
+          </li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
@@ -22,15 +44,34 @@
 </script>
 
 <style lang="scss" scoped>
-.main_list {
-  font-size: 18px;
-  margin-bottom: 23px;
-  color: #000;
+.main_item {
+  &:not(:first-child) {
+    margin-top: 5px;
+  }
+  
+
+  > a {
+    display: inline-block;
+    padding: 8px 0 9px;
+    font-size: 18px;
+    color: #000;
+  }
 }
 
 .sub_list {
-  font-size: 15px;
-  margin-bottom: 18px;
-  color: var(--j-gray400);
+  margin-top: 5px;
+}
+
+.sub_item {
+  &:not(:first-child) {
+    margin-top: 5px;
+  }
+
+  > a {
+    display: inline-block;
+    padding: 6px 0 7px;
+    font-size: 15px;
+    color: var(--j-gray400);
+  }
 }
 </style>
