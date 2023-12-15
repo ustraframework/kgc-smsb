9ed61a6b7a11306da0c5b5849abdc5a837f50c7b
hyangji <wco0289@gmail.com>
2023-12-15 16:42:28 +0900

    포인트 조회, 소멸 포인트 조회 PC 버전

--- node/fo/pages/pubs/MP/PI/UI_FU_0019_1.vue
+++ node/fo/pages/pubs/MP/PI/UI_FU_0019_1.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col items-center w-[650px]">
-    <h1 class="text-[30px] font-medium mb-[30px]">포인트 조회</h1>
+    <h1 class="text-[30px] font-medium mb-[30px]">소멸 포인트 조회</h1>
 
     <div class="w-[650px] bg-[#F4F6FA] p-[24px] rounded-xl">
       <div class="flex flex-col justify-center items-center mb-[20px] border-b border-[#E7E7E7] pb-4">
         <span class="text-[18px]">
-          <strong>홍길동</strong>님의 가용포인트
+          당월 소멸 예정 포인트
         </span>
         <span class="text-[30px] text-[#D20F27] mt-2">
           <strong class="mr-0.5">1,000</strong>P
@@ -14,17 +14,15 @@
 
       <div class="flex justify-between items-center mb-4">
         <div class="flex items-center">
-          <span class="text-[15px] font-medium mr-4">특별 포인트</span>
+          <span class="text-[15px] font-medium mr-4">12월 소멸 예정 포인트</span>
           <span class="text-[20px]">
             <strong class="mr-0.5">500</strong>P
           </span>
         </div>
-        <div class="flex items-center cursor-pointer" @click="$router.push('/pubs/MP/PI/UI_FU_0019_1')">
-          <span class="text-[15px] font-medium">
-            당월 소멸예정
+        <div class="flex items-center">
+          <span class="text-[15px] font-medium mr-4">
+            1월 소멸 예정 포인트
           </span>
-          <span class="text-[12px]">(전일기준)</span>
-          <img src="@/assets/images/svg/ico_more.svg" class="mr-4" alt="더보기"/>
           <span class="text-[20px]">
             <strong class="mr-0.5">1,000</strong>P
           </span>
@@ -33,60 +31,36 @@
 
       <div class="flex items-center">
         <img src="@/assets/images/svg/ico_info.svg" class="mr-1" alt="정보"/>
-        <span class="text-[12px] text-[#888888]">특별 포인트는 오프라인 매장에서만 사용 가능합니다.</span>
+        <span class="text-[12px] text-[#888888]">
+          향후 <strong>3개월</strong> 이내 소멸 예정인 포인트 합계 내역만 조회됩니다.
+        </span>
       </div>
     </div>
 
-    <div class="w-full flex mt-[40px]">
-      <SelectButton
-        v-model="selectedValue"
-        :options="selectOptions"
-        optionLabel="label"
-        optionValue="value"
-        class=" w-[310px]"
-      />
-      <div class="p-calendar-group">
-        <Calendar v-model="startDate" placeholder="2023-01-01" dateFormat="yy-mm-dd"  />
-        <span class="range">~</span>
-        <Calendar v-model="endDate" placeholder="2023-01-01" dateFormat="yy-mm-dd" />
-      </div>
-    </div>
-
-    <div class="w-full flex justify-between mt-[40px]">
-      <span class="text-[15px]">
-        최근 3개월 ∙ 전체
-      </span>
-
-      <Dropdown class="simple-dropdown w-1/4" v-model="dropdownValue" :options="['전체', 'selectA', 'selectB', 'selectC']">
-        <template #dropdownicon>
-          <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 8.5L9.64645 14.1464C9.84171 14.3417 10.1583 14.3417 10.3536 14.1464L16 8.5" stroke="#222222" stroke-width="1.2" stroke-linecap="round"/>
-          </svg>
-        </template>
-      </Dropdown>
+    <div class="w-full flex mt-[40px] justify-center text-[15px]">
+      특별 포인트 소멸 예정 포인트 내역입니다.
     </div>
 
     <div class="w-full flex flex-col rounded-xl border border-[#E7E7E7] mt-5 px-8 py-2">
       <div v-for="(item, i) in list" :key="i" class="flex justify-between py-6" :class="[i !== 0 ? ' border-t border-[#E7E7E7]' : '']">
         <div class="flex flex-col">
           <span class="text-[18px] font-bold">{{item.title}}</span>
-          <span class="text-[14px] text-[#666666] mt-2.5">{{item.date}}</span>
+          <span class="text-[14px] text-[#666666] mt-2.5">{{`유효기간 만료일: ${item.date}`}}</span>
         </div>
-        <div class="flex flex-col text-right" :class="[item.pointType === 'plus' ? 'text-[#D20F27]' : '']">
-          <span class="text-[14px] font-normal">{{item.type}}</span>
-          <span class="text-[18px] mt-0.5">
+        <div class="flex flex-col text-right justify-center" :class="[item.pointType === 'plus' ? 'text-[#D20F27]' : '']">
+          <span class="text-[18px]">
             <strong class="mr-0.5">{{item.point}}</strong>P
           </span>
         </div>
       </div>
     </div>
 
-    <Button class="more_btn">
+    <!-- <Button class="more_btn">
       <span class="mr-0.5">더보기</span>
       <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.2278 4.71875C10.2278 4.25276 9.85006 3.875 9.38407 3.875C8.91808 3.875 8.54032 4.25276 8.54032 4.71875V9.04032H4.21875C3.75276 9.04032 3.375 9.41808 3.375 9.88407C3.375 10.3501 3.75276 10.7278 4.21875 10.7278H8.54032V15.0492C8.54032 15.5152 8.91808 15.8929 9.38407 15.8929C9.85006 15.8929 10.2278 15.5152 10.2278 15.0492V10.7278H14.5492C15.0152 10.7278 15.3929 10.3501 15.3929 9.88407C15.3929 9.41808 15.0152 9.04032 14.5492 9.04032H10.2278V4.71875Z" fill="#758EBC"/>
       </svg>
-    </Button>
+    </Button> -->
 
   </div>
 </template>
@@ -103,11 +77,11 @@ interface ListType {
 }
 
 const list: ListType[] = [
-  { title: '케어나우', date: '2023-09-27 01:02', type: '활동 적립', pointType: 'plus', point: '+1,000' },
-  { title: '정관장몰', date: '2023-09-27 01:02', type: '구매 사용', pointType: 'minus', point: '-100' },
-  { title: '대치점', date: '2023-09-27 01:02', type: '영수증 적립', pointType: 'plus', point: '+10' },
-  { title: '정관장', date: '2023-09-27 01:02', type: '특별 포인트', pointType: 'plus', point: '+1,000' },
-  { title: '케어나우', date: '2023-09-27 01:02', type: '유효기간 만료로 소멸', pointType: 'minus', point: '-1,000' },
+  { title: '특별 포인트', date: '2023-11-30', type: '활동 적립', pointType: 'minus', point: '-1,000' },
+  { title: '특별 포인트', date: '2023-11-30', type: '구매 사용', pointType: 'minus', point: '-1,000' },
+  { title: '특별 포인트', date: '2023-11-30', type: '영수증 적립', pointType: 'minus', point: '-1,000' },
+  { title: '특별 포인트', date: '2023-11-30', type: '특별 포인트', pointType: 'minus', point: '-1,000' },
+  { title: '특별 포인트', date: '2023-11-30', type: '유효기간 만료로 소멸', pointType: 'minus', point: '-1,000' },
 ]
 
 
