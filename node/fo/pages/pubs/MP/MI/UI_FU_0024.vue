c84304dd7aaee7bfdfe691e5bbbca14e47cac982
gs-jang <gh.jang@gsitm.com>
2023-12-15 14:10:09 +0900

    fo 기념일관리 페이지 추가

--- node/fo/pages/pubs/MP/MI/UI_FU_0024.vue
+++ node/fo/pages/pubs/MP/MI/UI_FU_0024.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center w-[650px]">
+    <h1 class="text-[30px] font-medium mb-[30px]">기념일 관리</h1>
+    <div class="w-full bg-[#F4F6FA] p-[24px] rounded-[12px]">
+      <div class="flex justify-between items-center">
+        <span class="text-[15px]">나의 기념일을 등록해보세요. 최대 7개까지 가능합니다.</span>
+        <Button label="기념일 등록"/>
+      </div>
+    </div>
+
+    <div class="flex flex-col items-center mt-[80px]">
+      <img src="@/assets/images/svg/ico_warning_gray.svg" class="mb-[20px]">
+      <span class="text-[#888888] text-[16px] font-medium">등록된 기념일이 없습니다.</span>
+    </div>
 
   </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  layout: 'side',
+})
 </script>
 
 <style scoped>
-
 </style>
--- node/fo/pages/pubs/MP/MI/UI_FU_0025.vue
+++ node/fo/pages/pubs/MP/MI/UI_FU_0025.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center w-[650px]">
+    <h1 class="text-[30px] font-medium mb-[30px]">기념일 관리</h1>
+    <div class="w-full bg-[#F4F6FA] p-[24px] rounded-[12px]">
+      <div class="flex items-center justify-center">
+        <span class="text-[15px]">자녀는 최대 3명까지 가능하며 그 외에는 1명만 가능합니다.</span>
+      </div>
+    </div>
+
+    <div class="flex flex-col items-center mt-[40px]">
+      <div class="grid grid-cols-2 gap-[px]">
+        
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  layout: 'side',
+})
 </script>
 
 <style scoped>
-
 </style>
