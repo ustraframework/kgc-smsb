b46d8f0c5b326b47f1d999daddc986ca28485921
김명욱 <anko@gsitm.com>
2023-12-19 17:56:23 +0900

    회원가입 - 탈퇴된 회원, 이미 가입된 회원 페이지 요청 건

---
+++ node/fo/pages_mobile/pubs/MS/MJ/UI_FU_0000_1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col py-[60px] px-[20px] my-auto">
+    <div class="flex flex-col items-center">
+      <i>
+        <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_1080_7357)">
+            <circle cx="26" cy="26" r="26" fill="#D20F27" />
+            <path
+              d="M16.5 26.125L23.0231 32.6481C23.2177 32.8427 23.533 32.8435 23.7285 32.6498L36.5 20"
+              stroke="white"
+              stroke-width="3.5"
+              stroke-linecap="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_1080_7357">
+              <rect width="52" height="52" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </i>
+      <p class="mt-[20px] text-2xl"><strong>탈퇴 회원</strong>이십니다.</p>
+      <p class="mt-[12px] text-center text-sm">
+        재가입은 90일 이후에 가능합니다.<br />
+        고객센터로 문의해주세요.
+      </p>
+      <Button class="w-[160px] mt-[40px]" label="확인" size="large" outlined />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
