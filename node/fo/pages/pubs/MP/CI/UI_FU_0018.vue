f3724807c19d44de8e22c7d1535496f25a6e7f92
hyangji <wco0289@gmail.com>
2023-12-20 09:35:49 +0900

    Mobile 쿠폰조회 화면

--- node/fo/pages/pubs/MP/CI/UI_FU_0018.vue
+++ node/fo/pages/pubs/MP/CI/UI_FU_0018.vue
@@ -2,17 +2,17 @@
   <div class="flex flex-col items-center w-[650px]">
     <h1 class="text-[30px] font-medium mb-[30px]">쿠폰 조회</h1>
 
-    <div class="w-[650px] bg-[#F4F6FA] p-[16px] rounded-xl text-center">
+    <div class="w-[650px] flex bg-[#F4F6FA] p-[16px] rounded-xl text-center justify-center">
       정관장 온오프라인에서 발행한 쿠폰을 조회하실 수 있습니다.
     </div>
 
     <div class="w-full mt-[40px]">
       <TabView class="default_tab" v-model:activeIndex="activeTab">
 
-        <!-- 사용가능한 쿠폰 -->
-        <TabPanel header="사용가능한 쿠폰">
+        <!-- 사용가능 쿠폰 -->
+        <TabPanel header="사용가능 쿠폰">
           <div class="flex justify-between mb-[20px]">
-            <span> {{ `총 ${availableList.length}건` }}</span>
+            <span className="text-[15px] font-medium"> {{ `총 ${availableList.length}건` }}</span>
             <div class="coupon__search">
               <span
                 v-for="(type, i) in selectType"
@@ -62,7 +62,7 @@
         <!-- 사용완료/기간만료 쿠폰 -->
         <TabPanel header="사용완료/기간만료 쿠폰">
           <div class="flex justify-between mb-[20px]">
-            <span> {{ `총 ${unavailableList.length}건` }}</span>
+            <span className="text-[15px] font-medium"> {{ `총 ${unavailableList.length}건` }}</span>
             <div class="coupon__search">
               <span
                 v-for="(type, i) in selectType"
@@ -177,17 +177,15 @@ const unavailableList = [
 ]
 
 const selectType = [
-  { label: '전체', value: 'all'},
   { label: '온라인', value: 'online'},
   { label: '오프라인', value: 'offline'},
 ]
 
 const selected = ref({
-  'available': 'all',
-  'unavailable': 'all'
+  'available': 'online',
+  'unavailable': 'online'
 })
 
-// searchType: 'all' , 'online', 'offline'
 const onClickSearchType = (tabType, searchType) => {
   selected.value[tabType] = searchType;
 }
@@ -223,16 +221,12 @@ definePageMeta({
 .coupon__list {
   display:flex;
   flex-wrap: wrap;
+  gap: 16px;
   .coupon__item {
     padding: 20px;
-    margin-bottom: 16px;
     background-image: url("data:image/svg+xml,%3Csvg width='317' height='255' viewBox='0 0 317 255' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cmask id='path-1-inside-1_1162_6826' fill='white'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M0 8C0 3.58172 3.58172 0 8 0H221.397C221.397 6.62742 226.804 12 233.473 12C240.143 12 245.549 6.62742 245.549 0H309C313.418 0 317 3.58172 317 8V247C317 251.418 313.418 255 309 255L246.556 255C246.556 248.373 241.149 243 234.479 243C227.81 243 222.403 248.373 222.403 255L8 255C3.58173 255 0 251.418 0 247V8Z'/%3E%3C/mask%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M0 8C0 3.58172 3.58172 0 8 0H221.397C221.397 6.62742 226.804 12 233.473 12C240.143 12 245.549 6.62742 245.549 0H309C313.418 0 317 3.58172 317 8V247C317 251.418 313.418 255 309 255L246.556 255C246.556 248.373 241.149 243 234.479 243C227.81 243 222.403 248.373 222.403 255L8 255C3.58173 255 0 251.418 0 247V8Z' fill='white'/%3E%3Cpath d='M221.397 0H222.397V-1H221.397V0ZM245.549 0V-1H244.549V0H245.549ZM309 255V254V255ZM246.556 255H245.556V256L246.556 256V255ZM222.403 255V256L223.403 256V255H222.403ZM8 255L8 254H8V255ZM8 -1C3.02944 -1 -1 3.02944 -1 8H1C1 4.13401 4.13401 1 8 1V-1ZM221.397 -1H8V1H221.397V-1ZM233.473 11C227.35 11 222.397 6.06918 222.397 0H220.397C220.397 7.18566 226.257 13 233.473 13V11ZM244.549 0C244.549 6.06918 239.596 11 233.473 11V13C240.689 13 246.549 7.18566 246.549 0H244.549ZM309 -1H245.549V1H309V-1ZM318 8C318 3.02944 313.971 -1 309 -1V1C312.866 1 316 4.13401 316 8H318ZM318 247V8H316V247H318ZM309 256C313.971 256 318 251.971 318 247H316C316 250.866 312.866 254 309 254V256ZM246.556 256L309 256V254L246.556 254V256ZM247.556 255C247.556 247.814 241.695 242 234.479 242V244C240.603 244 245.556 248.931 245.556 255H247.556ZM234.479 242C227.264 242 221.403 247.814 221.403 255H223.403C223.403 248.931 228.356 244 234.479 244V242ZM8 256L222.403 256V254L8 254L8 256ZM-1 247C-1 251.971 3.02944 256 8 256V254C4.13401 254 1 250.866 1 247H-1ZM-1 8V247H1V8H-1Z' fill='%23E7E7E7' mask='url(%23path-1-inside-1_1162_6826)'/%3E%3C/svg%3E%0A");
     background-repeat: no-repeat;
     width: calc(50% - 8px);
-
-    &:nth-child(odd) {
-      margin-right: 16px;
-    }
 
     .coupon__item-top {
       display: flex;
