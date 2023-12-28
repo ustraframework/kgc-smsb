f60cf4441318315a2868cc6006f83302163663ea
yiseulll <yiseul@gsitm.com>
2023-12-28 16:56:26 +0900

    발송관리 수정

--- node/bo/pages/docs/index.vue
+++ node/bo/pages/docs/index.vue
@@ -245,7 +245,7 @@ const items = ref([
   { col1: '회원', col2: '발송관리', col3: '방송 채널(시스템)관리', col4: 'UI_BM_3401', col5: '/pubs/SM/SE/UI_BM_3401_pubs', col6: 'grid-form', col7: 'O' },
   { col1: '회원', col2: '발송관리', col3: '방송 채널(시스템)관리 - 이력보기', col4: 'UI_BM_3402', col5: '/pubs/SM/SE/UI_BM_3402_pubs', col6: 'popup-grid', col7: 'O' },
   { col1: '회원', col2: '발송관리', col3: '방송 템플릿 관리', col4: 'UI_BM_3403', col5: '/pubs/SM/SE/UI_BM_3403_pubs', col6: 'grid-form-grid-vertical', col7: 'O' },
-  { col1: '회원', col2: '발송관리', col3: '방송 템플릿 관리 - 미리보기', col4: 'UI_BM_3404', col5: '', col6: '퍼블리싱에서 처리', col7: 'X' },
+  { col1: '회원', col2: '발송관리', col3: '방송 템플릿 관리 - 미리보기', col4: 'UI_BM_3404', col5: '/pubs/SM/SE/UI_BM_3404_pubs', col6: '퍼블리싱에서 처리', col7: 'O' },
   { col1: '회원', col2: '발송관리', col3: '방송 템플릿 관리 - 파라미터(대체문자) 선택', col4: 'UI_BM_3405', col5: '/pubs/SM/SE/UI_BM_3405_pubs', col6: 'popup-grid', col7: 'O' },
   { col1: '회원', col2: '발송관리', col3: '회원 피로도 기준 관리', col4: 'UI_BM_3406', col5: '/pubs/SM/SE/UI_BM_3406_pubs', col6: 'grid-form', col7: 'X' },
   { col1: '회원', col2: '발송관리', col3: '피로도 기준 이력', col4: 'UI_BM_3407', col5: '/pubs/SM/SE/UI_BM_3407_pubs', col6: 'popup-grid', col7: 'X' },
