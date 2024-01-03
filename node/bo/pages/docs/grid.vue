87776454b8d43e144cbdcb83353ad90327a180e6
gs-jang <gh.jang@gsitm.com>
2024-01-03 09:57:05 +0900

    pagination 커스텀

--- node/bo/pages/docs/grid.vue
+++ node/bo/pages/docs/grid.vue
@@ -11,18 +11,25 @@
         </WjFlexGrid>
 
         <h1 class="mt-10 mb-4">Grid Pagination</h1>
-        <WjFlexGrid style="height: 256px" :initialized="grid.initialize">
-          <WjFlexGridColumn header="No" binding="col1" width="*" />
-          <WjFlexGridColumn header="지급일자" binding="col1" width="*" />
-          <WjFlexGridColumn header="지급방법" binding="col1" width="*" />
-          <WjFlexGridColumn header="지급사유" binding="col1" width="*" />
+        <UButton type="primary" text="Search" @click="() => grid.pagination.loadPageData()"></UButton>
+        <UButton type="primary" text="Init" @click="() => (grid.pagination.currentPageNo.value = 0)"></UButton>
+
+        <WjFlexGrid class="mt-2" :initialized="grid.initialize" style="height: 300px">
+          <WjFlexGridColumn binding="procId" header="프로세스아이디" />
+          <WjFlexGridColumn binding="reqId" header="요청아이디" />
+          <WjFlexGridColumn binding="reqNo" header="요청번호" />
+          <WjFlexGridColumn binding="sysCd" header="시스템" :cellTemplate="ctx => useUstraCodeValue('SYS_CD', ctx.value)" />
+          <WjFlexGridColumn binding="url" header="URL" />
+          <WjFlexGridColumn binding="httpMethCd" header="Method" />
         </WjFlexGrid>
 
+
         <UPaginationBar
-          v-model="grid.pagination.currentPageNo.value"
-          :pageSize="grid.pagination.pageSize.value"
-          :totalRecords="grid.pagination.totalRecords.value"
-        />
+        v-model="grid.pagination.currentPageNo.value"
+        :pageSize="grid.pagination.pageSize.value"
+        :totalRecords="grid.pagination.totalRecords.value"
+        :showStartEndButtons="true"
+      />
       </div>
 
       <!-- search input -->
@@ -55,8 +62,26 @@ definePageMeta({
 
 const grid = useWijmoFlexGrid({
   pagination: {
-    modelValue: 1,
-    totalRecords: 100,
+    handler(currentPage, pageSize, orders) {
+      return useOnError(
+        async () => {
+          const service = useUstraInterfaceService()
+          const result = await service.getIntefaceHistories({
+            header: {
+              currentPage,
+              pageSize,
+              orders,
+            },
+            searchValue: {},
+          })
+          return {
+            data: result.body,
+            totalRecords: result.header.totalRecords,
+          }
+        },
+        { message: '데이터 조회 중 오류가 발생하였습니다.' },
+      )()
+    },
     pageSize: 20,
   },
 })
